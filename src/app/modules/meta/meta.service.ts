b83586af4fa0b95f430f909fa6ac8f1e8b5b16f0
Imranahmed935 <imrantahir9918@gmail.com>
2025-11-12 22:09:27 +0600

    server code almost done

--- src/app/modules/meta/meta.service.ts
+++ src/app/modules/meta/meta.service.ts
@@ -1,29 +1,29 @@
 import { PaymentStatus, UserRole } from "@prisma/client";
 import { IJWTPayload } from "../../types/common";
+import httpStatus from 'http-status'
+import ApiError from "../../errors/ApiError";
 import { prisma } from "../../shared/prisma";
 
-const fetchDashboardMetaData = async(user:IJWTPayload)=>{
-    let metaData;
 
+const fetchDashboardMetaData = async (user: IJWTPayload) => {
+    let metadata;
     switch (user.role) {
         case UserRole.ADMIN:
-            metaData = "admin meta data loaded"
+            metadata = await getAdminMetaData();
             break;
-
         case UserRole.DOCTOR:
-            metaData =await getDoctorMetaData(user)
+            metadata = await getDoctorMetaData(user);
             break;
-
         case UserRole.PATIENT:
-            metaData= "patient meta data loaded"
-    
-        default:
+            metadata = await getPatientMetaData(user);
             break;
+        default:
+            throw new ApiError(httpStatus.BAD_REQUEST, "Invalid user role!")
     }
 
-    return metaData;
+    return metadata;
+};
 
-}
 
 const getDoctorMetaData = async (user: IJWTPayload) => {
     const doctorData = await prisma.doctor.findUniqueOrThrow({
@@ -84,6 +84,114 @@ const getDoctorMetaData = async (user: IJWTPayload) => {
         formattedAppointmentStatusDistribution
     }
 }
+
+const getPatientMetaData = async (user: IJWTPayload) => {
+    const patientData = await prisma.patient.findUniqueOrThrow({
+        where: {
+            email: user?.email
+        }
+    });
+
+    const appointmentCount = await prisma.appointment.count({
+        where: {
+            patientId: patientData.id
+        }
+    });
+
+    const prescriptionCount = await prisma.prescription.count({
+        where: {
+            patientId: patientData.id
+        }
+    });
+
+    const reviewCount = await prisma.review.count({
+        where: {
+            patientId: patientData.id
+        }
+    });
+
+    const appointmentStatusDistribution = await prisma.appointment.groupBy({
+        by: ['status'],
+        _count: { id: true },
+        where: {
+            patientId: patientData.id
+        }
+    });
+
+    const formattedAppointmentStatusDistribution = appointmentStatusDistribution.map(({ status, _count }) => ({
+        status,
+        count: Number(_count.id)
+    }))
+
+    return {
+        appointmentCount,
+        prescriptionCount,
+        reviewCount,
+        formattedAppointmentStatusDistribution
+    }
+}
+
+
+const getAdminMetaData = async () => {
+    const patientCount = await prisma.patient.count();
+    const doctorCount = await prisma.doctor.count();
+    const adminCount = await prisma.admin.count();
+    const appointmentCount = await prisma.appointment.count()
+    const paymentCount = await prisma.payment.count()
+
+    const totalRevenue = await prisma.payment.aggregate({
+        _sum: {
+            amount: true
+        },
+        where: {
+            status: PaymentStatus.PAID
+        }
+    })
+
+    const barChartData = await getBarChartData();
+    const pieChartData = await getPieChartData();
+
+    return {
+        patientCount,
+        doctorCount,
+        adminCount,
+        appointmentCount,
+        paymentCount,
+        totalRevenue,
+        barChartData,
+        pieChartData
+    }
+
+}
+
+
+const getBarChartData = async () => {
+    const appointmentCountPerMonth = await prisma.$queryRaw`
+        SELECT DATE_TRUNC('month', "createdAt") AS month,
+        CAST(COUNT(*) AS INTEGER) AS count
+        FROM "appointments"
+        GROUP BY month
+        ORDER BY month ASC
+    `
+
+    return appointmentCountPerMonth
+}
+
+const getPieChartData = async () => {
+    const appointmentStatusDistribution = await prisma.appointment.groupBy({
+        by: ['status'],
+        _count: { id: true }
+    });
+
+    const formatedAppointmentStatusDistribution = appointmentStatusDistribution.map(({ status, _count }) => ({
+        status,
+        count: Number(_count.id)
+    }));
+
+    return formatedAppointmentStatusDistribution;
+}
+
+
 export const MetaService = {
     fetchDashboardMetaData
 }
