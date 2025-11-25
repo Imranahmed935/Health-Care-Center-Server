499f50a1db1a045f82d97c204a433c8175a9c597
Imranahmed935 <imrantahir9918@gmail.com>
2025-11-25 22:35:01 +0600

    appointment functionality added

--- src/app/modules/appointment/appointment.controller.ts
+++ src/app/modules/appointment/appointment.controller.ts
@@ -5,6 +5,8 @@ import sendResponse from "../../shared/sendResponse";
 import { IJWTPayload } from "../../types/common";
 import { AppointmentService } from "./appointment.service";
 import pick from "../../helper/pick";
+import { appointmentFilterableFields } from "./appointment.constant";
+import httpStatus from 'http-status'
 
 
 const createAppointment = catchAsync(async (req: Request & { user?: IJWTPayload }, res: Response) => {
@@ -17,6 +19,19 @@ const createAppointment = catchAsync(async (req: Request & { user?: IJWTPayload 
         message: "Appointment created successfully!",
         data: result
     })
+});
+
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+    const filters = pick(req.query, appointmentFilterableFields)
+    const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+    const result = await AppointmentService.getAllFromDB(filters, options);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Appointment retrieval successfully',
+        meta: result.meta,
+        data: result.data,
+    });
 });
 
 const getMyAppointment = catchAsync(async (req: Request & { user?: IJWTPayload }, res: Response) => {
@@ -51,5 +66,6 @@ const updateAppointmentStatus = catchAsync(async (req: Request & { user?: IJWTPa
 export const AppointmentController = {
     createAppointment,
     getMyAppointment,
-    updateAppointmentStatus
+    updateAppointmentStatus,
+    getAllFromDB
 }
